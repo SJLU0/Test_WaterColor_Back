ccabe1177fd011c2fe3f399a7664f725646f9668
SJLU0 <sjlu78713@gmail.com>
2024-12-26 11:15:22 +0800

    Initial commit

---
+++ src/main/java/com/example/testwc/TestwcApplication.java
@@ -0,0 +1,14 @@
+package com.example.testwc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestwcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestwcApplication.class, args);
+		System.out.println("這是test_watercolor網站");
+	}
+
+}
---
+++ src/test/java/com/example/testwc/TestwcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testwc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestwcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
